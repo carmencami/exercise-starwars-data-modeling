3c4764455e02fbdf7299535251206162218db786
carmencami <carmengreta3@outlook.es>
2022-08-20 10:58:14 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -28,24 +28,24 @@ class Planets(Base):
     planet_description = Column(String(250), nullable=False)
    
 class Character(Base):
-    __tablename__ = 'Character'
+    __tablename__ = 'character'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    character_name = Column(String(250), nullable=False)
-    character_description = Column(String(250), nullable=False)
+    Character_name = Column(String(250), nullable=False)
+    Character_description = Column(String(250), nullable=False)
 
 class Favorites(Base):
-    __tablename__ = 'Favorites'
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship("User")
     planets_id = Column(Integer, ForeignKey('planets.id'))
-    Planets = relationship(Planets)
-    character_id = Column(Integer, ForeignKey('character.id'))
-    character = relationship(Character)
+    Planets = relationship("Planets")
+    Character_id = Column(Integer, ForeignKey('character.id'))
+    Character = relationship("Character")
     
 
     def to_dict(self):
